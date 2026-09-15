4ed82c76cb0bc42edef3b1b00cc2517089d04d05
mayararosarodrigues-oss <mayara.rosa.rodrigues@escola.pr.gov.br>
2026-09-15 14:29:04 -0300

    Update arrayUnidade.js

--- arrayUnidade.js
+++ arrayUnidade.js
@@ -1,1 +1,8 @@
+var unidades= [“Segurança da Informação”, “Game Designer”, “Ciência da Computação”];
+
+function unidadeEscolhida(numero){
+return unidades[numero];
+}
+
+console.log(unidadeEscolhida(0));
 
